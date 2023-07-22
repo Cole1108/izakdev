c62f5c19a74efe73891cbcaa2eba08546e054025
Cole1108 <139693008+Cole1108@users.noreply.github.com>
2023-07-22 18:13:25 -0400

    Create music.js

---
+++ music.js
@@ -0,0 +1,9 @@
+<script>
+  // Wait for the page to fully load before playing the music
+  window.addEventListener('load', function() {
+    // Get the audio element
+    var audio = document.getElementById('background-music');
+    // Play the audio
+    audio.play();
+  });
+</script>
