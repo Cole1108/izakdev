71acc85c61824c6f391d857e96ad6c41e3b10f6b
Cole1108 <139693008+Cole1108@users.noreply.github.com>
2023-07-22 18:33:44 -0400

    Update and rename music.js to script.js

--- music.js
+++
@@ -1,9 +0,0 @@
-<script>
-  // Wait for the page to fully load before playing the music
-  window.addEventListener('load', function() {
-    // Get the audio element
-    var audio = document.getElementById('background-music');
-    // Play the audio
-    audio.play();
-  });
-</script>
---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+window.addEventListener('load', function() {
+  var audio = document.getElementById('background-music');
+  audio.play();
+});
